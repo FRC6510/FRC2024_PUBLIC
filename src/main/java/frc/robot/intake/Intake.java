4c946edfb4fb7721702e736e22a6ff7a25488242
Froze-N-Milk <oscar.chevalier@pymblelc.nsw.edu.au>
2024-01-07 16:13:23 +1100

    squash for release

---
+++ src/main/java/frc/robot/intake/Intake.java
@@ -0,0 +1,49 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.intake;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+public class Intake extends SubsystemBase {
+  /** Creates a new Intake. */
+  private static final int NOTE_DETECTOR_DIO_CHANNEL = 9;
+  private final DigitalInput noteDetector;
+  private static final int INTAKE_CTRE_BUS = 17;
+  public TalonFX intakeMotor;
+  private double interruptTime;
+
+  public Intake() {
+    this.noteDetector = new DigitalInput(NOTE_DETECTOR_DIO_CHANNEL);
+    this.intakeMotor = new TalonFX(INTAKE_CTRE_BUS, "rio");
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  public Command spinIntake(double power){
+    return new SpinIntake(this, power);
+  }
+
+  public boolean getSensorState() {
+    return noteDetector.get();
+  }
+
+  public double currentTime(){
+    return System.currentTimeMillis();
+  }
+  
+  public void keepSpinningTime(){
+    interruptTime = currentTime();
+    while((currentTime()- interruptTime) < 3000){
+      intakeMotor.set(0.3);
+    }
+    intakeMotor.set(0);
+  }
+}
---
+++ src/main/java/frc/robot/intake/SpinIntake.java
@@ -0,0 +1,41 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.intake;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class SpinIntake extends Command {
+  private final Intake intake;
+  private final double power;
+  /** Creates a new SpinIntake. */
+  public SpinIntake(Intake intake, double power) {
+    this.intake = intake;
+    this.power = power;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(intake);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+      intake.intakeMotor.set(power);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    intake.keepSpinningTime();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return !intake.getSensorState();
+  }
+}
