3b8dce1bed9032a883a0de95b58ac41f5e79abf3
Froze-N-Milk <oscar.chevalier@pymblelc.nsw.edu.au>
2024-01-07 16:13:23 +1100

    squash for release

---
+++ src/main/java/frc/robot/lift/LiftPID.java
@@ -0,0 +1,50 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.lift;
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class LiftPID extends Command {
+  private final Lift lift;
+  private double rotations;
+  private double maxRotations = -90;
+  private double minRotations = 0;
+
+  /** Creates a new LiftPID. */
+  public LiftPID(Lift lift, double rotations) {
+    this.lift = lift;
+    this.rotations = rotations;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(lift);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    // if (rotations > maxRotations) rotations = maxRotations;
+    // if (rotations < minRotations) rotations = minRotations;
+    lift.setLiftPosition(rotations);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    lift.liftMotor.set(0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    //  if (lift.liftMotor.getPosition().getValueAsDouble() > maxRotations || lift.liftMotor.getPosition().getValueAsDouble() < minRotations){
+    //   return true;
+    //  }
+    //  else{
+      return false;
+    //  }
+  }
+}
